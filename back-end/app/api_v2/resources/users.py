53290f595659aa31a5640bbf9da5c4c016e17424
haihong.wang <296701193@qq.com>
2019-11-05 14:37:44 +0800

    添加user的CRUD API

--- back-end/app/api_v2/resources/users.py
+++ back-end/app/api_v2/resources/users.py
@@ -7,3 +7,25 @@
 @time: 2019/11/5 14:05
 @desc:
 """
+from flask_restful import Resource
+
+
+# 针对单个用户的注册，删除，查询
+class UserAPI(Resource):
+    def get(self, id):
+        pass
+
+    def delete(self, id):
+        pass
+
+    def put(self, id):
+        pass
+
+
+# 一堆用户的查询，注册
+class UserListAPI(Resource):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
--- back-end/app/api_v2/urls.py
+++ back-end/app/api_v2/urls.py
@@ -9,6 +9,9 @@
 """
 from app.api_v2.app import api
 from app.api_v2.resources.ping import Ping
+from app.api_v2.resources.users import UserAPI, UserListAPI
 
 # 统一注册路由
 api.add_resource(Ping, '/', '/ping')
+api.add_resource(UserAPI, '/users', endpoint='users')
+api.add_resource(UserAPI, '/users/<int:id>', endpoint='user')
